cc7ee6d35e7d35da181021d33e22ed9cd9777be5
Alpalhao <alpalhao@gmail.com>
2016-08-23 23:01:39 +0100

    Start repository

---
+++ ReturnOfTheCarrot/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ReturnOfTheCarrot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbHotKey = new System.Windows.Forms.TextBox();
+            this.tbCommand = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(398, 136);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hot key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Command";
+            // 
+            // tbHotKey
+            // 
+            this.tbHotKey.Location = new System.Drawing.Point(16, 79);
+            this.tbHotKey.Name = "tbHotKey";
+            this.tbHotKey.Size = new System.Drawing.Size(205, 20);
+            this.tbHotKey.TabIndex = 3;
+            this.tbHotKey.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbHotKey_KeyDown);
+            this.tbHotKey.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbHotKey_KeyUp);
+            // 
+            // tbCommand
+            // 
+            this.tbCommand.Location = new System.Drawing.Point(239, 78);
+            this.tbCommand.Name = "tbCommand";
+            this.tbCommand.Size = new System.Drawing.Size(269, 20);
+            this.tbCommand.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 192);
+            this.Controls.Add(this.tbCommand);
+            this.Controls.Add(this.tbHotKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Return of the Carrot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbHotKey;
+        private System.Windows.Forms.TextBox tbCommand;
+    }
+}
+
---
+++ ReturnOfTheCarrot/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReturnOfTheCarrot
+{
+    public partial class Form1 : Form
+    {
+        private List<Keys> _KeysPressed = new List<Keys>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbHotKey_KeyUp(object sender, KeyEventArgs e)
+        {
+           
+            e.Handled = true;
+        }
+
+        private void tbHotKey_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_KeysPressed.Contains(e.KeyCode))
+            {
+                _KeysPressed.Add(e.KeyCode);
+                PrintKeys();
+                e.Handled = true;
+            }
+        }
+
+        private void PrintKeys()
+        {
+            tbHotKey.Text = string.Join("+", _KeysPressed);
+        }
+
+    }
+}
